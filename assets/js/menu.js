2720b9bbd9da3e2b6da8772c76421e57992560bc
Rubens Gabriel <rubens-ita@hotmail.com.br>
2023-08-28 16:08:10 -0300

    parte inicial do menu

---
+++ assets/js/menu.js
@@ -0,0 +1,24 @@
+/*================= MENU RESPONSIVO =================*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+            toggle.classList.toggle('bx-x')
+        })
+    }
+}
+
+showMenu('header-toggle','nav-menu')
+
+/*================= Ativar e remover menu =================*/
+const navLink = document.getElementById('.nav__link')
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
